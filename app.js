006c56929f363c2ad3c3f183bbc4e56b61b3662b
Phumthawan Nokun <knzphumthawan@gmail.com>
2025-05-25 13:32:41 +0700

    update mongoose connection

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    authSource: "admin"
 }, function(err) {
     if (err) {
         console.log("error!! " + err)
